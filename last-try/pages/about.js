28fa26d7b96f624624af67c0bb7a2bc18bcf5ee4
Henzolina <henokrobale@gmail.com>
2022-10-25 10:38:38 -0500

    page-added-commit

--- last-try/pages/about.js
+++ last-try/pages/about.js
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 export default function About() {
     return (
+    <>
     <h1>About Page</h1>
+    <h2>
+        <Link href="/">Back to Home</Link>
+    </h2>
+    </>
     )
   }
